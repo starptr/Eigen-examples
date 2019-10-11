bd50d52e83beabf9a16954d714bec4db6255952b
RyuseiTaniguchi <55517061+RyuseiTaniguchi@users.noreply.github.com>
2019-10-11 13:36:58 -0700

    Update main.cpp

--- Eigen-examples/main.cpp
+++ Eigen-examples/main.cpp
@@ -11,26 +11,28 @@ int main()
 	All of these functions are declared and defined in examples.h and examples.cpp files.
 	*/
 	
-	//make a 2*2 matrix
-	matrix2_2(1.0, 2.0, 2.5, 7.0);
-	vector2(2.0,3.0)
-	
-	
+	exInstance();
+	exMultiply();
+		
         //creates metrixes m which has 2 rows and 2 columns
         MatrixXd m(2, 2);
-        m << 1, 3,
-            5, 7;
+        m(0,0) = 1;
+        m(1,0) = 3;
+        m(0,1) = 5;
+        m(1,1) = 7;
 
         MatrixXd n(2, 2);
         n << 1, 2,
              3, 4;
 	
+	//creates vector v which is 2 dimensions
+        VectorXd v(2);
+        v(0) = 2.5;
+        v(1) = 5;
+	
 	//show the metrix m
         std::cout << "Here is a matrix m:\n" <<  m << std::endl <<std::endl;
 	std::cout << "Here is a matrix n:\n" <<  n << std::endl <<std::endl;
-	
-	exInstance();
-	exMultiply();
 	
 	//culculate matrix + matrix
 	addition(m,n);
@@ -38,5 +40,11 @@ int main()
 	scalarMultiplication(2.5, m);
 	//calculate matrix * matrix
 	matrixMultiplication(m,m);
+	
+	//show the vector v
+	std::cout << "Here is the vector v:\n" << v << std::endl << std::endl;
+	
+        //solve the system of the linear equation
+        linearEqua(m, v);
 	
 }
