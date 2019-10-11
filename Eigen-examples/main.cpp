4d9183e1662e8c52122328d5dd6693c890851b50
RyuseiTaniguchi <55517061+RyuseiTaniguchi@users.noreply.github.com>
2019-10-11 13:00:59 -0700

    Update main.cpp

--- Eigen-examples/main.cpp
+++ Eigen-examples/main.cpp
@@ -11,14 +11,18 @@ int main()
 	All of these functions are declared and defined in examples.h and examples.cpp files.
 	*/
 	
-       //creates metrixes m which has 2 rows and 2 columns
-       MatrixXd m(2, 2);
-       m << 1, 3,
+	//make a 2*2 matrix
+	matrix2_2(double 1, double 2, double 2.5, double 7);
+	
+	
+        //creates metrixes m which has 2 rows and 2 columns
+        MatrixXd m(2, 2);
+        m << 1, 3,
             5, 7;
 
-       MatrixXd n(2, 2);
-       n << 1, 2,
-            3, 4;
+        MatrixXd n(2, 2);
+        n << 1, 2,
+             3, 4;
 	
 	//show the metrix m
         std::cout << "Here is a matrix m:\n" <<  m << std::endl <<std::endl;
@@ -33,4 +37,5 @@ int main()
 	scalarMultiplication(2.5, m);
 	//calculate matrix * matrix
 	matrixMultiplication(m,m);
+	
 }
