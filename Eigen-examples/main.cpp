606c72a195b77c51ca68babfecaee7aa41c72f70
RyuseiTaniguchi <55517061+RyuseiTaniguchi@users.noreply.github.com>
2019-10-11 21:04:29 -0700

    Update main.cpp

--- Eigen-examples/main.cpp
+++ Eigen-examples/main.cpp
@@ -15,31 +15,5 @@ int main()
 	exAddition();
 	exScalarMultiply();
 	exMultiply();
-		
-        //creates metrixes m which has 2 rows and 2 columns
-        MatrixXd m(2, 2);
-        m(0,0) = 1;
-        m(1,0) = 3;
-        m(0,1) = 5;
-        m(1,1) = 7;
-
-        MatrixXd n(2, 2);
-        n << 1, 2,
-             3, 4;
-	
-	//creates vector v which is 2 dimensions
-        VectorXd v(2);
-        v(0) = 2.5;
-        v(1) = 5;
-	
-	//show the metrix m
-        std::cout << "Here is a matrix m:\n" <<  m << std::endl <<std::endl;
-	std::cout << "Here is a matrix n:\n" <<  n << std::endl <<std::endl;
-	
-	//show the vector v
-	std::cout << "Here is the vector v:\n" << v << std::endl << std::endl;
-	
-        //solve the system of the linear equation
-        linearEqua(m, v);
-	
+	solveLinearEq();
 }
