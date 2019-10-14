44aa00e3245e8c5ecced8421af740c8955df40ec
Yuto Nishida <yuyu.nishida@gmail.com>
2019-10-13 22:05:16 -0700

    Define LinearTransformation as geometrical interpretation of matricies

--- Eigen-examples/LinearTransformation.cpp
+++ Eigen-examples/LinearTransformation.cpp
@@ -1,7 +1,69 @@
 #include "LinearTransformation.h"
 
-LinearTransformation::LinearTransformation() {
+LinearTransformation::LinearTransformation(int dim) {
+	refresh_T(Eigen::MatrixXd::Identity(dim, dim));
+}
 
+LinearTransformation::LinearTransformation(Eigen::MatrixXd A) {
+	refresh_T(A);
 }
 
 LinearTransformation::~LinearTransformation() {}
+
+void LinearTransformation::refresh_T(Eigen::MatrixXd T) {
+	this->T = T;
+	T_decomp.compute(T);
+	T_eigens.compute(T);
+}
+
+Eigen::MatrixXd LinearTransformation::getTransformation() {
+	return T;
+}
+
+void LinearTransformation::setTransformation(Eigen::MatrixXd T) {
+	refresh_T(T);
+}
+
+int LinearTransformation::getDimDomain() {
+	return T.rows();
+}
+
+int LinearTransformation::getDimCodomain() {
+	return T.cols();
+}
+
+int LinearTransformation::getNullity() {
+	return T_decomp.dimensionOfKernel();
+}
+
+int LinearTransformation::getRank() {
+	return T_decomp.rank();
+}
+
+Eigen::VectorXcd LinearTransformation::getEigenvals() {
+	return T_eigens.eigenvalues();
+}
+
+bool LinearTransformation::isInvertible() {
+	return T_decomp.isInvertible();
+}
+
+Eigen::MatrixXd LinearTransformation::getBasisOfKernel() {
+	return T_decomp.kernel();
+}
+
+Eigen::MatrixXd LinearTransformation::getBasisOfImage() {
+	return T_decomp.image(T);
+}
+
+Eigen::MatrixXcd LinearTransformation::getEigenvecs() {
+	return T_eigens.eigenvectors();
+}
+
+LinearTransformation LinearTransformation::getInverse() {
+	return LinearTransformation(T_decomp.inverse());
+}
+
+Eigen::MatrixXd LinearTransformation::transform(Eigen::MatrixXd x) {
+	return T * x;
+}
--- Eigen-examples/LinearTransformation.h
+++ Eigen-examples/LinearTransformation.h
@@ -2,13 +2,51 @@
 #define LINEAR_TRANSFORMATION
 
 #include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
 
 class LinearTransformation
 {
 	Eigen::MatrixXd T;
+	Eigen::FullPivLU<Eigen::MatrixXd> T_decomp;
+	Eigen::EigenSolver<Eigen::MatrixXd> T_eigens;
+
+	void refresh_T(Eigen::MatrixXd T);
+
 public:
-	LinearTransformation();
+	//dim is the dimension of the square identity transformation
+	LinearTransformation(int dim);
+	//A is the transformation matrix
+	LinearTransformation(Eigen::MatrixXd A);
 	~LinearTransformation();
+
+	//Returns T
+	Eigen::MatrixXd getTransformation();
+	//Assigns T and handles initial computations
+	void setTransformation(Eigen::MatrixXd T);
+
+	//Returns dimension of domain of T
+	int getDimDomain();
+	//Returns dimension of codomain of T
+	int getDimCodomain();
+	//Returns nullity (i.e. dimension of kernel) of T
+	int getNullity();
+	//Returns rank (i.e. dimension of image) of T
+	int getRank();
+	//Returns a complex column vector containing eigenvalues, repeated by algebraic multiplicity
+	Eigen::VectorXcd getEigenvals();
+	//Returns whether T has an inverse
+	bool isInvertible();
+	//Returns a matrix whose columns form a basis of the kernel of T
+	Eigen::MatrixXd getBasisOfKernel();
+	//Returns a matrix whose columns form a basis of the image of T
+	Eigen::MatrixXd getBasisOfImage();
+	//Returns a matrix whose columns are the complex eigenvectors with corresponding eigenvalues
+	Eigen::MatrixXcd getEigenvecs();
+	
+	//Returns the inverse of T, if it exists
+	LinearTransformation getInverse();
+	//x is the input matrix to be transformed
+	Eigen::MatrixXd transform(Eigen::MatrixXd x);
 };
 
 #endif // !LINEAR_TRANSFORMATION
